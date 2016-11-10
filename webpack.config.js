f6ff1c852030ceb47ecfa67e91477638249bacc6
chenyuan <chenyuanluck@163.com>
2016-11-10 10:32:04 +0800

    初始化webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,17 +4,18 @@ let ExtractTextPlugin = require('extract-text-webpack-plugin');
 // 分离css插件配置
 let extractSCSS = new ExtractTextPlugin('stylesheets/[name].css');
 module.exports = {
-    entry: './src/entry.js',                // 演示单入口文件
+    entry: './src/entry.js',                                        // 演示单入口文件
     output: {
-        path: path.join(__dirname, 'main'),          // 打包输出的路径
-        filename: 'bundle.js',                      // 打包输出的文件名
-        publicPath: './main/'                        // html引用路径
+        path: path.join(__dirname, 'main'),                         // 打包输出的路径
+        filename: 'bundle.js',                                      // 打包输出的文件名
+        publicPath: './main/'                                       // html引用路径
     },
     module: {
         loaders: [
             {
                 test: /\.scss$/,
-                loader: extractSCSS.extract(['css','sass?sourceMap'])
+                // loader: extractSCSS.extract(['css?sourceMap', 'sass?sourceMap'])
+                loader: 'style!css?sourceMap!sass?sourceMap'
             }
         ]
     },
